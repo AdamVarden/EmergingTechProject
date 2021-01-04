dbcb270680ebd2283d1f0a1484b9f6e8fa6882b9
ADAM VARDEN <G00359605@gmit.ie>
2021-01-04 15:42:32 +0000

    Added graph to model notebook, tested different epochs on model, added code to possibly import the model in web-service.py, edited dockerfile and requirements.txt

--- web-service.py
+++ web-service.py
@@ -1,7 +1,11 @@
+# https://www.tensorflow.org/guide/keras/save_and_serialize - loading the model
 # flask for web app.
 import flask as fl
 # numpy for numerical work.
 import numpy as np
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 # Create a new web app.
 app = fl.Flask(__name__)
@@ -9,4 +13,6 @@ app = fl.Flask(__name__)
 # Add root route.
 @app.route("/")
 def home():
+  
+  data_model = keras.models.load_model("model")
   return app.send_static_file('index.html')
