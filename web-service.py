fc3880c380f5a8e47bce824efbe94c25e498ea2b
ADAM VARDEN <G00359605@gmit.ie>
2021-01-05 20:19:52 +0000

    Added more functionaility to web-service.py to work in loading the saved model and working on to make a prediction and send it to index.html. Worked on index.html gave input field

--- web-service.py
+++ web-service.py
@@ -1,6 +1,8 @@
 # https://www.tensorflow.org/guide/keras/save_and_serialize - loading the model
+
 # flask for web app.
-import flask as fl
+from flask import Flask ,request, render_template
+
 # numpy for numerical work.
 import numpy as np
 # Tensorflow for keras
@@ -8,11 +10,20 @@ import tensorflow as tf
 from tensorflow import keras
 
 # Create a new web app.
-app = fl.Flask(__name__)
+app = Flask(__name__)
 
 # Add root route.
 @app.route("/")
 def home():
+    
+  return render_template('index.html')
+
+@app.route("/send", methods=['POST', 'GET'])
+def send():
+  predict_data = 0
+  if request.method == 'POST':
+    prediction = float(request.form.get('prediction'))
+    data_model = keras.models.load_model("model.h5")
+    predict_data= data_model.predict(prediction)
   
-  data_model = keras.models.load_model("model.h5")
-  return app.send_static_file('index.html')
+  return render_template('index.html', predict_data = predict_data)
