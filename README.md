75bb47edaa6c3b266a8f7d7af499280be3717a1f
ADAM VARDEN <G00359605@gmit.ie>
2021-01-06 20:46:45 +0000

    Added comments to web-service.py and added to README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Emerging Tech Project
+Adam Varden - G00359605
+
+
+## Running the web-service
+### Run the following commands in the command prompt.
+> set FLASK_APP=web-service.py
+
+> python -m flask run
+
+### View the index.html in browser
+> Copy this link and enter into url line in brower:
+> http://127.0.0.1:5000/
--- web-service.py
+++ web-service.py
@@ -18,12 +18,28 @@ def home():
     
   return render_template('index.html')
 
+# Making a route to get the input from form in index.html
 @app.route("/send", methods=['POST', 'GET'])
 def send():
   predict_data = 0
+  
+  # To verify the request is a post
   if request.method == 'POST':
-    prediction = float(request.form.get('prediction'))
+    
+    dataset = pd.read_csv("https://raw.githubusercontent.com/ianmcloughlin/2020A-machstat-project/master/dataset/powerproduction.csv")
+    
+    # Adding the prediction input from index.html
+    prediction = np.array(float(request.form.get('prediction')))
+    
+    dataset_features = dataset.copy()
+    dataset_features = np.array(dataset_features)
+    dataset_power = dataset_features.pop('power')
+    dataset_power = np.array(dataset_power)
+    
+    # Loading the saved model
     data_model = keras.models.load_model("model.h5")
+
+    # Making a prediction
     predict_data= data_model.predict(prediction)
   
   return render_template('index.html', predict_data = predict_data)
